0f3898905454748ef8289ef9d6894942b9bb4eeb
RobAxt <roberto.axt@gmail.com>
2020-05-17 22:40:01 -0300

    addAlarm update

--- lib/AlarmInterface/AlarmInterface.cpp
+++ lib/AlarmInterface/AlarmInterface.cpp
@@ -45,8 +45,8 @@ AlarmInterface::loopInterface(void) {
 
 int 
 AlarmInterface::addAlarm(AlarmInterface::alarmSettings &currentAlarm) {
-    int tmpId = 256;
-    if(currentAlarm.enable == true) 
+    int tmpId = 258;
+    if(currentAlarm.enable == true || (currentAlarm.enable == false && strcmp(currentAlarm.dayOfWeek,"none") != 0 && strcmp(currentAlarm.fxName,"none") != 0) ) 
         if(strcmp(currentAlarm.dayOfWeek,"all") == 0) 
             tmpId = Alarm.alarmRepeat(currentAlarm.hour, currentAlarm.minute, 0, findFX(currentAlarm.fxName));
         else if(strcmp(currentAlarm.dayOfWeek,"sunday") == 0) 
@@ -64,31 +64,42 @@ AlarmInterface::addAlarm(AlarmInterface::alarmSettings &currentAlarm) {
                                       else if(strcmp(currentAlarm.dayOfWeek,"saturday") == 0) 
                                                tmpId = Alarm.alarmRepeat(dowSaturday, currentAlarm.hour, currentAlarm.minute, 0, findFX(currentAlarm.fxName));
                                            else if(strcmp(currentAlarm.dayOfWeek,"none") == 0) 
-                                                   tmpId = 257;
+                                                   tmpId = 256;
                                                 else 
-                                                    tmpId =258;
-    else
-        Homie.getLogger() << "  ◦ Current Alarm not enable (id:" << currentAlarm.id << ")" << endl;
+                                                    tmpId =257;
 
-    if(tmpId < 255) {
-        _settings[tmpId].id = tmpId;
-        _settings[tmpId].enable = currentAlarm.enable;
-        _settings[tmpId].hour = currentAlarm.hour;
-        _settings[tmpId].minute = currentAlarm.minute;
-        strlcpy(_settings[tmpId].dayOfWeek, currentAlarm.dayOfWeek, strlen(currentAlarm.dayOfWeek)+1);
-        strlcpy(_settings[tmpId].fxName, currentAlarm.fxName, strlen(currentAlarm.fxName)+1);
+    switch(tmpId) {
+        case 255:
+        case 256: 
+        case 257: {
+            Homie.getLogger() << "  ✖ Fail to add alarm,(id:" << currentAlarm.id <<" code:" << tmpId << ")" << endl;
+            _settings[currentAlarm.id].id = currentAlarm.id;
+            _settings[currentAlarm.id].enable = false;
+            _settings[currentAlarm.id].hour = 0;
+            _settings[currentAlarm.id].minute = 0;
+            strlcpy(_settings[currentAlarm.id].dayOfWeek, "none", 5);
+            strlcpy(_settings[currentAlarm.id].fxName, "none", 5);
+            break;
+        }
+        case 258: {
+            Homie.getLogger() << "  ✖ Current Alarm not used (id:" << currentAlarm.id << ")" << endl;
+            break;
+        }
+        default: {
+            _settings[tmpId].id = tmpId;
+            _settings[tmpId].enable = currentAlarm.enable;
+            _settings[tmpId].hour = currentAlarm.hour;
+            _settings[tmpId].minute = currentAlarm.minute;
+            strlcpy(_settings[tmpId].dayOfWeek, currentAlarm.dayOfWeek, strlen(currentAlarm.dayOfWeek)+1);
+            strlcpy(_settings[tmpId].fxName, currentAlarm.fxName, strlen(currentAlarm.fxName)+1);
 
-        Homie.getLogger() << "  ✔ Alarm added (id: " << _settings[tmpId].id  << ")" <<  endl;
-        
-    } else {
-        Homie.getLogger() << "  ✖ Fail to add alarm,(id:" << currentAlarm.id <<" code:" << tmpId << ")" << endl;
-        _settings[currentAlarm.id].id = currentAlarm.id;
-        _settings[currentAlarm.id].enable = false;
-        _settings[currentAlarm.id].hour = 0;
-        _settings[currentAlarm.id].minute = 0;
-        strlcpy(_settings[currentAlarm.id].dayOfWeek, "none", 5);
-        strlcpy(_settings[currentAlarm.id].fxName, "none", 5);
+            if(currentAlarm.enable == false) 
+                Alarm.disable(tmpId);
+
+            Homie.getLogger() << "  ✔ Alarm added (id: " << _settings[tmpId].id  << ")" <<  endl;
+        }
     }
+
     return tmpId;
 }
 int
@@ -194,10 +205,11 @@ AlarmInterface::saveJSONSettings(void) {
      for(int i=0; i < dtNBR_ALARMS ;i++) {
         json["Alarms"][i]["id"] = _settings[i].id;
         json["Alarms"][i]["enable"] = _settings[i].enable ? "true" : "false";
-        json["Alarms"][i]["hour"] = _settings[i].minute;;
+        json["Alarms"][i]["hour"] = _settings[i].hour;;
         json["Alarms"][i]["minute"] = _settings[i].minute;
         json["Alarms"][i]["dayOfWeek"] = _settings[i].dayOfWeek;
         json["Alarms"][i]["fxName"] = _settings[i].fxName;
     }
-    serializeJson(json, jsonFile); serializeJson(json, Serial);   Homie.getLogger() << endl;
+    serializeJson(json, jsonFile);
+    serializeJson(json, Serial);   Homie.getLogger() << endl;
 }
